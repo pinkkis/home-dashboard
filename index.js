2407a803a6e3621265454fecf67c86e04bea7fb7
Kristian Koivisto-Kokko <pinkkis@gmail.com>
2016-10-20 22:35:27 +0100

    storing temps in memory, and allowing rename

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+// store temps
+let temperatures = {};
 
 // mqtt client
 const mqttBrokerUrl = process.env.MQTT_BROKER || 'mqtt://192.168.192.150';
@@ -23,7 +25,21 @@ mqttClient.on('connect', function () {
 mqttClient.on('message', function (topic, message) {
 	// message is Buffer
 	console.log('MQTT message: ', topic, message.toString());
-	io.emit(topic, JSON.parse(message.toString()));
+
+	let data = JSON.parse(message.toString());
+
+	data.forEach( t => {
+		if (!temperatures[t.id]) {
+			temperatures[t.id] = t;
+		} else {
+			Object.assign(temperatures[t.id], t);
+		}
+	});
+
+	let temps = Object.keys(temperatures).map(t => temperatures[t]);
+	console.log('temps conversion\n', temps, temperatures);
+
+	io.emit(topic, temps);
 });
 
 // express
@@ -34,22 +50,25 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
+app.post('/rename/:id', (req, res) => {
+	console.log(req.params.id, req.body.name);
+	if (temperatures[req.params.id]) {
+		temperatures[req.params.id].name = req.body.name;
+	}
+
+	res.status(200).end();
+});
+
 server.listen(httpPort, () => {
 	console.log('Express listening on port:' + httpPort);
 });
-
-
 
 // socket io
 
 io.on('connection', (socket) => {
 	console.info('Socket.io client connected');
 
-	socket.emit('news', { hello: 'world' });
-
-	socket.on('my other event', (data) => {
-		console.log(data);
-	});
+	socket.emit('init', Object.keys(temperatures).map(t => temperatures[t]));
 });
 
 // process and error handling
